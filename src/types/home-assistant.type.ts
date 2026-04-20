2e56bb3aa3331140d57fc4205028f7fb98c90e9f
Kasper Wissendorf <kw2011@live.dk>
2026-04-20 19:03:09 +0200

    feat: add themes property to HomeAssistant interface for enhanced theme management

--- src/types/home-assistant.type.ts
+++ src/types/home-assistant.type.ts
@@ -1,3 +1,8 @@
 export interface HomeAssistant {
 	states?: Record<string, { state: string }>;
+	themes?: {
+		theme: string | null;
+		darkMode: boolean;
+		themes: Record<string, Record<string, string>>;
+	};
 }
